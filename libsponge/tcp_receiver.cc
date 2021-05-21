757c31206a12d7a438c3c2323aa4bf101ea2d65b
100ask <user@100ask.com>
2021-05-21 07:21:42 -0400

    lab2-completed

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,59 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    bool ret = false;
+    static size_t abs_seqno = 0;
+    size_t length;
+    if (seg.header().syn) {
+        if (_syn_flag) {        // already get a SYN, refuse other SYN.
+            return;
+        }
+        _syn_flag = true;
+        ret = true;
+        _isn = seg.header().seqno.raw_value();
+        abs_seqno = 1;
+        _base = 1;
+        length = seg.length_in_sequence_space() - 1;
+        if (length == 0) {      // segment's content only have a SYN flag
+            return;
+        }
+    } else if (!_syn_flag) {    // before get a SYN, refuse any segment
+        return;
+    } else {                    // not a SYN segment, compute it's abs_seqno
+        //拆包成绝对地址（64位 初始位是0），同时获得该seg的index
+        abs_seqno = unwrap(WrappingInt32(seg.header().seqno.raw_value()),WrappingInt32(_isn), abs_seqno);
+        length = seg.length_in_sequence_space();
+    }
+
+    if (seg.header().fin) {
+        if(_fin_flag) {         // already get a FIN, refuse other FIN
+            return ;
+        }
+        _fin_flag = true;
+        ret = true;
+    }
+    // not FIN and not one size's SYN, check border
+    else if (seg.length_in_sequence_space() == 0 && abs_seqno == _base){    //空包？
+        return;
+    } else if (abs_seqno >= _base + window_size() || abs_seqno + length <= _base) {
+        if (!ret) {
+            return;
+        }
+    }
+    _reassembler.push_substring(seg.payload().copy(), abs_seqno - 1, seg.header().fin);
+    _base = _reassembler.head_index() + 1;
+    if (_reassembler.input_ended())  // FIN be count as one byte
+        _base++;
+    return;
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (_base > 0)
+        return WrappingInt32(wrap(_base, WrappingInt32(_isn)));
+    else
+        return std::nullopt;
+ }
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { 
+    return _capacity - _reassembler.stream_out().buffer_size();
+ }
