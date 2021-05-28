6c6f7bf00dd8197eaaa6c629f475fe44955d3093
100ask <user@100ask.com>
2021-05-28 07:04:51 -0400

    lab5-completed

--- apps/webget.cc
+++ apps/webget.cc
@@ -17,7 +17,7 @@ void get_URL(const string &host, const string &path) {
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
-    CS144TCPSocket sock{};
+    FullStackSocket sock{};
     sock.connect(Address(host,"http"));
     string input("GET "+path+" HTTP/1.1\r\nHost: "+host+"\r\n\r\n");
     sock.write(input);
